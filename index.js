6071f3c5ba8a780530ad8fd6aee547e4110470f0
Thomas Hibbard <thomashibbard@Thomass-MacBook-Pro.local>
2016-09-28 20:09:15 -0500

    refining functionality to list all files

--- index.js
+++ index.js
@@ -1,76 +1,108 @@
 "use strict"
 
-let yargs = require('yargs')
+const yargs = require('yargs').argv
 	, path = require('path')
 	, fs = require('fs')
 	, Promise = require('bluebird')
 	, escape = require('escape-regexp')
-	, isDirectory = require('is-directory');
+	, isImage = require('is-image')
+	, junk = require('junk');
 
- // Promise.promisifyAll(fs)
-
-let res = [];
-function ls(source, firstRun, callback) {
-	firstRun = firstRun || false;
+let res = {};
+let lt = function(source, parentDistance, callback) {
+	// firstRun = firstRun || false;
+	parentDistance = parentDistance || 0;
 	callback = callback || function(){};
 	source = path.resolve(path.sep + source);
-	// fs.readdir(source, (err, items) => {
-	// 	items.forEach((item, i, items) => {
-	// 		var pathAndItem = path.join(source, item);
-	// 		var pathAndItemLen = pathAndItem.split(path.sep).filter(Boolean).length;
-	// 		var distanceFromBase = pathAndItemLen - baseDirLen;
-	// 		// console.log(baseDirLen, pathAndItemLen, distanceFromBase);
-	// 		var isDir = fs.lstatSync(pathAndItem).isDirectory();
-	// 		if(isDir){
-	// 			// console.log(baseDir + '\n' + item)			
-	// 			console.log('-'.repeat(distanceFromBase), '📁', item);
-	// 			ls(pathAndItem);
-	// 		}else{
-	// 			// console.log(baseDir + '\n' + item)
-	// 			console.log('-'.repeat(distanceFromBase), '📄', item);
-	// 		}
-	// 	})
-	// });
 
+	let items = fs.readdirSync(source);
+	items = items.filter(junk.not)
 
-	var items = fs.readdirSync(source)
+	items.forEach((item, i, items) => {
+		let pathAndItem = path.join(source, item);
+		let pathAndItemLen = pathAndItem.split(path.sep).filter(Boolean).length;
+		let distanceFromBase = pathAndItemLen - baseDirLen;
+		let leader;
+		let isDir = isDirectory(pathAndItem)
+		let icon = getIcon(pathAndItem);
 
-// console.log('items', items)
-			items.forEach((item, i, items) => {
-				var pathAndItem = path.join(source, item);
-				var pathAndItemLen = pathAndItem.split(path.sep).filter(Boolean).length;
-				var distanceFromBase = pathAndItemLen - baseDirLen;
-				// console.log(baseDirLen, pathAndItemLen, distanceFromBase);
-				var isDir = fs.lstatSync(pathAndItem).isDirectory();
-				if(isDir){
-					// console.log(baseDir + '\n' + item)			
-					console.log('-'.repeat(distanceFromBase), '📁', item);
-					ls(pathAndItem);
-				}else{
-					// console.log(baseDir + '\n' + item)
-					console.log('-'.repeat(distanceFromBase), '📄', item);
-				}
-			})
+		if(isDir){
+			leader = getVisualIndexIdentifier(distanceFromBase);
+			console.log(leader, '» '.repeat(distanceFromBase - 1), icon.icon, item);
+			lt(pathAndItem, distanceFromBase);
+		}else{
+			leader = getVisualIndexIdentifier(distanceFromBase - 1);
+			// console.log(distanceFromBase)
+			console.log(leader, '» '.repeat(distanceFromBase - 1), icon.icon, item);
+		}
+	})
+}
+function getIcon(path){
+  
+	let isImageFlag = isImage(path);
+	let isDirFlag = isDirectory(path);
+	var ret;
+  if(isImageFlag){
+  	ret = {type: 'image', icon: '🗻 '};
+  }else if(isDirFlag){
+  	ret = {type: 'dir', icon: '📁 '};
+  }else{
+  	ret = {type: 'file', icon: '📄 '};
+  }
+  return ret;
+}
 
-	
+function isDirectory(item){
+	return fs.lstatSync(item).isDirectory();
+}
 
+function getItemType(item){
+	let ret;
 
+	if(isImage(item)){
+		ret = {type: 'image', icon: '🗻 '};
+	}else if(isDir(item)){
+		ret = {type: 'dir', icon: '📁 '};
+	}else if(isFile(item)){
+		ret = {type: 'file', icon: '📄 '};
+	}
+}
 
+function getFileType(path){
+
+}
+
+function getVisualIndexIdentifier(distanceFromBase){
+	let leader = '';
+	if (distanceFromBase === 1){
+		//leader = '├─';
+	}	else{
+		//leader = '└─'
+	}
+	return leader;
 }
 
 function dirTest(item){	
 	fs.lstatSync(item).isDirectory();
 }
 
-var baseDir = '/Users/thomashibbard/Desktop/ls-recursive/testDirectory';
-var baseDirLen = baseDir.split(path.sep).filter(Boolean).length;
-// console.log(baseDirArr);no
-ls(baseDir, true, function(){
+let baseDir = yargs.d || '/Users/thomashibbard/Desktop/ls-recursive/testDirectory';
+let baseDirLen = baseDir.split(path.sep).filter(Boolean).length;
+
+lt(baseDir, 0, function(){
 	processResults();
 });
-function processResults(){
 
-	// console.log(res);
+function isFirstChild(){
+
+}
+
+function isMiddleChild(){
+
+}
+
+function isLastChild(){
+
 }
 
 
@@ -83,4 +115,6 @@ function sort(a, b){
     return 0;
   };
 }
+
+module.exports = lt;
 
--- readdir.js
+++ readdir.js
@@ -1,6 +1,14 @@
 var recursive = require('recursive-readdir');
+var rdr = require('readdir-recursive-promise');
+var directoryContents = require('directory-contents');
 
-recursive('/Users/thomashibbard/Desktop/ls-recursive/testDirectory', function (err, files) {
-  // Files is an array of filename
-  console.log(files);
+var dir = '/Users/thomashibbard/Desktop/ls-recursive/testDirectory';
+// recursive('/Users/thomashibbard/Desktop/ls-recursive/testDirectory', function (err, files) {
+//   // Files is an array of filename
+//   console.log(files);
+// });
+
+directoryContents(dir, function(err, contents) {
+	if (err) throw err;
+	console.log(contents);
 });
